5ae032cabc118d83a669478d39703d7aefb9b98b
ankit-kumar-pathak <ak8672053@gmail.com>
2021-04-01 10:43:36 +0530

    Local Storage Added
    Signed-off-by: ankit-kumar-pathak <ak8672053@gmail.com>

--- public/script.js
+++ public/script.js
@@ -5,27 +5,63 @@ let btnCleanup = $('#btnCleanup')
 let btnSort = $('#btnSort')
 let inpNewTask = $('#inpNewTask')
 
+let curTasks=JSON.parse(localStorage.getItem("tasks"));
+
+if(curTasks){
+    
+    for(let t of curTasks){
+      if(t.done===true){
+        let listItem=$('<li>',{
+            'class':'list-group-item done',
+            text:t.task
+        })
+           listItem.click(() => {
+        listItem.toggleClass('done');
+         updateLS();
+    })
+        ulTasks.append(listItem);
+       }
+        else if(t.done===false){
+         let listItem=$('<li>',{
+            'class':'list-group-item',
+            text:t.task
+        })
+           listItem.click(() => {
+        listItem.toggleClass('done');
+         updateLS();
+    })
+        ulTasks.append(listItem);
+       }
+    }
+    toggleInputButtons();
+}
 //ADD ITEM TO LIST
 function addItem() {
     let listItem = $('<li>', {
         'class': 'list-group-item',
         text: inpNewTask.val()
     })
+
     ulTasks.append(listItem);
     listItem.click(() => {
         listItem.toggleClass('done');
+         updateLS();
     })
     inpNewTask.val('');
     toggleInputButtons();
+   updateLS();
 }
 
 function clearDone() {
     $('#ulTasks .done').remove();
     toggleInputButtons();
+    updateLS();
 }
 
 function sortTasks() {
     $('#ulTasks .done').appendTo(ulTasks);
+
+    updateLS();
 }
 //ADDING ITEM ON PRESSING ENTER KEY
 inpNewTask.keypress((e) => {
@@ -53,6 +89,17 @@ btnCleanup.click(clearDone);
 
 btnSort.click(sortTasks);
 
+function updateLS(){
+    let taskContainer=[];
+    let tasks=document.querySelectorAll('li');
+    for(t of tasks){
+        taskContainer.push({
+            task:t.innerText,
+            done:t.classList.contains('done')
+        })
+    }
+   localStorage.setItem("tasks",JSON.stringify(taskContainer));
+}
 
 /*
 $('<li>',{
